2f568759437f03385b8b197bc7f5ab5bf17bc5e8
minijaham <60860894+minijaham@users.noreply.github.com>
2021-01-27 11:09:19 +0900

    Added CPS counter. Credit to @Inaayat04

--- src/minijaham/PvPUtilities/Main.php
+++ src/minijaham/PvPUtilities/Main.php
@@ -9,14 +9,20 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\plugin\Plugin;
 use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as C;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
+
 class Main extends PluginBase implements Listener
 {
+    private $clicks;
+    
     public $db;
     
     public function onEnable()
@@ -47,6 +53,44 @@ class Main extends PluginBase implements Listener
         }
     }
     
+    public function getCPS(Player $player): int{
+		if(!isset($this->clicks[$player->getLowerCaseName()])){
+			return 0;
+		}
+		$time = $this->clicks[$player->getLowerCaseName()][0];
+		$clicks = $this->clicks[$player->getLowerCaseName()][1];
+		if($time !== time()){
+			unset($this->clicks[$player->getLowerCaseName()]);
+			return 0;
+		}
+		return $clicks;
+	}
+	
+	public function addCPS(Player $player): void{
+		if(!isset($this->clicks[$player->getLowerCaseName()])){
+			$this->clicks[$player->getLowerCaseName()] = [time(), 0];
+		}
+		$time = $this->clicks[$player->getLowerCaseName()][0];
+		$clicks = $this->clicks[$player->getLowerCaseName()][1];
+		if($time !== time()){
+			$time = time();
+			$clicks = 0;
+		}
+		$clicks++;
+		$this->clicks[$player->getLowerCaseName()] = [$time, $clicks];
+	}
+
+	public function onDataPacketReceive(DataPacketReceiveEvent $event){
+		$player = $event->getPlayer();
+		$packet = $event->getPacket();
+		if($packet instanceof InventoryTransactionPacket){
+			$transactionType = $packet->transactionType;
+			if($transactionType === InventoryTransactionPacket::TYPE_USE_ITEM || $transactionType === InventoryTransactionPacket::TYPE_USE_ITEM_ON_ENTITY){
+				$this->addCPS($player);
+			}
+		}
+	}
+    
     public function registerCombo(EntityDamageEvent $event)
     {
         $entity = $event->getEntity();
@@ -61,7 +105,7 @@ class Main extends PluginBase implements Listener
             $this->db->set($damager->getName(), $this->db->get($damager->getName()) + 1);
             $this->db->set($entity->getName(), 0);
             
-            $damager->sendTip("Combo: ".$this->db->get($damager->getName())." | Reach: ".round($damager->distance($entity))); // Count for combo / reach
+            $damager->sendTip("CPS: ".$this->getCps($damager)." | Combo: ".$this->db->get($damager->getName())." | Reach: ".round($damager->distance($entity))); // Count for combo / reach
         }
     }
 }
